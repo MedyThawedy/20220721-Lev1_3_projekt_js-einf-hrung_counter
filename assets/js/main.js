e2d970e1406d5b5e0b29a39a09a8e90c8ef9b207
MD <dhmedy@googlemail.com>
2022-07-21 16:41:40 +0200

    lc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,32 @@
 
-let zahl = parseInt(document.getElementById("pResult").innerHTML)
+let v = parseInt(document.getElementById("pResult").innerHTML)
 console.log(zahl);
 
-function fn_add1() {
-    zahl = zahl + 1;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
+function fn_add1(zahl) {
+    v = v + zahl;
+    console.log(v)
+    document.getElementById("pResult").innerHTML = v;
     console.log(document.getElementById("pResult").innerHTML)
 }
 
-function fn_substract1() {
-    zahl = zahl - 1;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
-    console.log(document.getElementById("pResult").innerHTML)
-}
-
-function fn_add10() {
-    zahl = zahl + 10;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
-    console.log(document.getElementById("pResult").innerHTML)
-}
-
-function fn_substract10() {
-    zahl = zahl - 10;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
-    console.log(document.getElementById("pResult").innerHTML)
-}
-
-function fn_add100() {
-    zahl = zahl + 100;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
-    console.log(document.getElementById("pResult").innerHTML)
-}
-
-function fn_substract100() {
-    zahl = zahl - 100;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
+function fn_substract1(zahl) {
+    v = v - zahl;
+    console.log(v)
+    document.getElementById("pResult").innerHTML = v;
     console.log(document.getElementById("pResult").innerHTML)
 }
 
 
 function fn_reset() {
-    zahl = 0;
-    document.getElementById("pResult").innerHTML = zahl;
+    v = 0;
+    document.getElementById("pResult").innerHTML = v;
     console.log(document.getElementById("pResult").innerHTML)
 }
 
 function fn_d() {
-    zahl = zahl * 2;
-    console.log(zahl)
-    document.getElementById("pResult").innerHTML = zahl;
+    v = v * 2;
+    console.log(v)
+    document.getElementById("pResult").innerHTML = v;
     console.log(document.getElementById("pResult").innerHTML)
 }
 
